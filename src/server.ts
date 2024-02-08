85333242c2ac1aced5404b0b9279e0ca0d6eab6f
anurag47103 <anuragsharma47103@gmail.com>
2024-02-08 14:36:23 +0530

    Changed deployment code

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use('/check', (req, res) => {
-    res.status(200).send("Backend Working");
+    res.status(200).send("Backend Working....");
 })
 
 app.use('/api/v1/auth', authRoutes);
